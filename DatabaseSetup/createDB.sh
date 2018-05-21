dd0d26768c4666aaa2575ecf21efbdad622ef03f
Jack <jsim89@gmail.com>
2018-05-21 09:37:54 +0800

    feat(database): Added database creation scripts

---
+++ DatabaseSetup/createDB.sh
@@ -0,0 +1,15 @@
+echo "start setup"
+sqlcmd -i DBsetup.sql
+
+echo "Insert Categories"
+sqlcmd -i insertCategories.sql
+
+echo "Insert Authors"
+sqlcmd -i insertAuthors.sql
+
+echo "Insert Books"
+sqlcmd -i insertBooks.sql
+
+echo "Insert Discounts"
+sqlcmd -i insertDiscounts.sql
+
