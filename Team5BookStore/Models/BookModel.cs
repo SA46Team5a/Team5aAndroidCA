d1ec9e44531b29088a88f201e0747547e7145420
Jack <jsim89@gmail.com>
2018-05-22 12:16:35 +0800

    fix(BookModel): fixed ISBN checking conditions

--- Team5BookStore/Models/BookModel.cs
+++ Team5BookStore/Models/BookModel.cs
@@ -13,7 +13,7 @@ namespace Team5BookStore.Models
         static public Book GetBookByISBN(string ISBN)
         {
 
-            if (!Int32.TryParse(ISBN, out int x) || ISBN.Length == 13)
+            if (!Int64.TryParse(ISBN, out Int64 x) || !(ISBN.Length == 13))
                 throw new InvalidObjectParams("ISBN of book must be a 13-digit numeric string");
             else
             {
