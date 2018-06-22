f18a5cc195dbc0a991404106dfd5b3f1c58c3e78
Jack <jsim89@gmail.com>
2018-06-22 15:09:39 +0800

    feat(SearchActivity): added SearchActivity

--- app/src/main/java/com/example/anthony/androidca/SearchActivity.java
+++ app/src/main/java/com/example/anthony/androidca/SearchActivity.java
@@ -1,13 +1,41 @@
 package com.example.anthony.androidca;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SearchActivity extends Activity {
+    EditText searchTerm;
+    Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getActionBar().setTitle("Search Books");
+
+        setupViews();
+    }
+
+    private void setupViews() {
+        searchTerm = findViewById(R.id.searchTerm);
+        searchButton = findViewById(R.id.searchButton);
+
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(searchTerm, InputMethodManager.SHOW_IMPLICIT);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent searchIntent = new Intent(view.getContext(), ListOfBooksActivity.class);
+                searchIntent.putExtra("searchTerm", searchButton.getText().toString());
+                startActivity(searchIntent);
+            }
+        });
     }
 }
